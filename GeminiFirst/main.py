1820dac70d413c3bdbdfe0f45db9b2a1afaf41f6
Aasif Ansari <116907759+asif0808@users.noreply.github.com>
2026-03-14 23:20:40 +0530

    Update API key for genai client

--- GeminiFirst/main.py
+++ GeminiFirst/main.py
@@ -1,6 +1,6 @@
 from google import genai
 client=genai.Client(
-    api_key='AIzaSyAtNCJ_WndNEoeqagMtqK1oHiU5N7oIFds'
+    api_key='Gemini-Api-Key'
 )
 response=client.models.generate_content(
     model='gemini-2.5-flash',contents='Explain how ai works in fewer words'
